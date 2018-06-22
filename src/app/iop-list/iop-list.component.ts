c8671024537ca006593957b8816a2411f53ec48a
Pedro De Los Reyes <pedro.reyes@ad.infosys.com>
2018-06-22 12:47:24 -0500

    Initial IOP list implementation.

--- src/app/iop-list/iop-list.component.ts
+++ src/app/iop-list/iop-list.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IopListComponent implements OnInit {
 
+  mockData = Array();
+
   constructor() { }
 
   ngOnInit() {
+    /* Creating mock data with random values for IOP-List */
+    for(let i=0; i<10; i++) {
+      let tempDate = new Date(Date.now() - Math.floor(Math.random() * 8000000000))
+      let card = {
+        iopNumber : Math.floor(Math.random() * 3000),
+        date : tempDate.toString().substring(4, 15),
+        phase : Math.ceil(Math.random() * 4),
+        description: 'Description placeholder',
+        boolean: Math.random() > 0.7 ? true : false
+      }
+      this.mockData.push(card);
+    }
   }
-
 }
